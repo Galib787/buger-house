8f4265af366134ee434aced4ce7a444b21119f44
MD Galib Hossain <133055551+Galib787@users.noreply.github.com>
2024-11-08 01:40:33 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.activ-slider').slick({
+        infinite: true,
+        arrows:false,
+        dots:true,
+        autoplay:true,
+        centerPadding: '60px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      });
+})
